be1df16aa0600bd33039bf1d358cc9f9a048a129
raquel-cmps <raquelcampos@edu.unifil.br>
2025-04-05 11:44:08 -0300

    ajuste para melhorar o principio S

--- src/SSolid/Exemplo2/MainEncomenda.java
+++ src/SSolid/Exemplo2/MainEncomenda.java
@@ -3,8 +3,7 @@ package SSolid.Exemplo2;
 import java.util.Scanner;
 
 public class MainEncomenda {
-    public static void main(String[] args) {
-
+    public static Encomenda lerEncomenda() {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Digite o ID da encomenda: ");
             int idEncomenda = sc.nextInt();
@@ -12,13 +11,11 @@ public class MainEncomenda {
             System.out.println("Digite o peso (em kg): ");
             double peso = sc.nextDouble();
 
-            Encomenda encomenda = new Encomenda(idEncomenda, peso);
-
-            Exportador.salvarEmArquivo(encomenda);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            return new Encomenda(idEncomenda, peso);
         }
-
+    }
+    public static void main(String[] args) {
+        Encomenda encomenda = lerEncomenda();
+        Exportador.salvarEmArquivo(encomenda);
     }
 }
